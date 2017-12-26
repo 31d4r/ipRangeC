4288ab7136fc84442bfdef7827cf79df314018cc
Eldar Tutnjic <eldartutnjic@Eldars-MacBook-Pro.local>
2017-12-26 04:16:33 +0100

    First upload

---
+++ ipRange/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  ipRange
+//
+//  Created by Eldar Tutnjic on 24/12/2017.
+//  Copyright © 2017 tutnjiceldar.com.ba. All rights reserved.
+//
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main (int argc, const char * argv[]) {
+    struct ifaddrs * ifAddrStruct=NULL;
+    struct ifaddrs * ifa=NULL;
+    void * tmpAddrPtr=NULL;
+    
+    getifaddrs(&ifAddrStruct);
+    
+    for (ifa = ifAddrStruct; ifa != NULL; ifa = ifa->ifa_next) {
+        if (!ifa->ifa_addr) {
+            continue;
+        }
+        if (ifa->ifa_addr->sa_family == AF_INET) { 
+            tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
+            char addressBuffer[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
+            printf("%s IP Address %s\n", ifa->ifa_name, addressBuffer);
+        }
+    }
+    if (ifAddrStruct!=NULL) freeifaddrs(ifAddrStruct);
+    return 0;
+}
